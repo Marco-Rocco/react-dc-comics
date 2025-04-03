305ca9bddecd81bf2a549c7fa9168191d4f78eaa
Marco-Rocco <marcorocco28@icloud.com>
2025-04-03 18:51:07 +0200

    feat: footer lists

---
+++ src/assets/FooterLogo.jsx
@@ -0,0 +1,9 @@
+import footerlogo from "../assets/dc-logo-bg.png";
+
+export default function FooterLogo() {
+    return (
+        <>
+            <img src={footerlogo} alt="" />
+        </>
+    )
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,63 @@
 export default function Footer() {
     return (
         <>
-            <p>patt</p>
+            <footer>
+                <div className="listsContainer">
+                    <div className="list">
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Video</li>
+                            <li>News</li>
+                        </ul>
+                    </div>
+
+                    <div className="list">
+                        <h3>SHOP</h3>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC collectibles</li>
+                        </ul>
+                    </div>
+
+                    <div className="list">
+                        <h3>DC</h3>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Video</li>
+                            <li>News</li>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Video</li>
+                            <li>News</li>
+                        </ul>
+                    </div>
+
+                    <div className="list">
+                        <h3>SITES</h3>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Megaine</li>
+                            <li>Shop DC</li>
+                            <li>Shop DC</li>
+                            <li>Shop DC</li>
+                            <li>Shop DC</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </footer >
         </>
     )
 }
