00d722d1433106b417bb0e5f266b75448ee260ce
Marco-Rocco <marcorocco28@icloud.com>
2025-04-03 14:55:04 +0200

    feat: create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+// import Header from "./components/Header.jsx";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
