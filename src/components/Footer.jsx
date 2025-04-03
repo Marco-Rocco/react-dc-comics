8513f87d513b5aae958d84706eb00f2e82bdd007
Marco-Rocco <marcorocco28@icloud.com>
2025-04-03 20:23:30 +0200

    feat: footer bottom-bar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,65 +4,76 @@ export default function Footer() {
     return (
         <>
             <footer>
-                <div className="listsContainer">
-                    <div className="list">
-                        <h3>DC COMICS</h3>
-                        <ul>
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Video</li>
-                            <li>News</li>
-                        </ul>
+                <div className="gigaBigContainer">
+                    <div className="listsContainer">
+                        <div className="list">
+                            <h3>DC COMICS</h3>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Video</li>
+                                <li>News</li>
+                            </ul>
+                        </div>
+
+                        <div className="list">
+                            <h3>SHOP</h3>
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC collectibles</li>
+                            </ul>
+                        </div>
+
+                        <div className="list">
+                            <h3>DC</h3>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Video</li>
+                                <li>News</li>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Video</li>
+                                <li>News</li>
+                            </ul>
+                        </div>
+
+                        <div className="list">
+                            <h3>SITES</h3>
+                            <ul>
+                                <li>DC</li>
+                                <li>MAD Megaine</li>
+                                <li>Shop DC</li>
+                                <li>Shop DC</li>
+                                <li>Shop DC</li>
+                                <li>Shop DC</li>
+                            </ul>
+                        </div>
                     </div>
 
-                    <div className="list">
-                        <h3>SHOP</h3>
-                        <ul>
-                            <li>Shop DC</li>
-                            <li>Shop DC collectibles</li>
-                        </ul>
-                    </div>
-
-                    <div className="list">
-                        <h3>DC</h3>
-                        <ul>
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Video</li>
-                            <li>News</li>
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Video</li>
-                            <li>News</li>
-                        </ul>
-                    </div>
-
-                    <div className="list">
-                        <h3>SITES</h3>
-                        <ul>
-                            <li>DC</li>
-                            <li>MAD Megaine</li>
-                            <li>Shop DC</li>
-                            <li>Shop DC</li>
-                            <li>Shop DC</li>
-                            <li>Shop DC</li>
-                        </ul>
-                    </div>
+                    <figure>
+                        <FooterLogo />
+                    </figure>
                 </div>
 
-                <figure>
-                    <FooterLogo />
-                </figure>
+                <div className="bottomBar">
+                    <button>SIGN-UP NOW!</button>
 
+                    <div className="social">
+                        <span>FOLLOW US</span>
+
+                        <img src="../public/img/footer-facebook.png" alt="" />
+                    </div>
+                </div>
             </footer >
         </>
     )
