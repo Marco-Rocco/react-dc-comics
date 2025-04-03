a6334eae35575fb8393137ef27b2fb4586d6d96d
Marco-Rocco <marcorocco28@icloud.com>
2025-04-03 15:38:26 +0200

    feat: create header, make it flex

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
+import Logo from "../assets/Logo"
+
 export default function Header() {
     return (
         <>
-            <p>NONNT</p>
+            <header>
+                <figure>
+                    <Logo />
+                </figure>
+
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </header>
         </>
     )
 }
