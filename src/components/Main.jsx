5474349e51dbee75c1aa76a20dcd981b09c8e5ae
Marco-Rocco <marcorocco28@icloud.com>
2025-04-03 16:18:13 +0200

    fix: move img in public folder + cleate main nav

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,19 @@ export default function Main() {
     return (
         <>
             <main>
-                <p> <strong>Content goes here</strong></p>
+                <p>
+                    <strong>Content goes here</strong>
+                </p>
+
+                <nav>
+                    <ul>
+                        <li><img src="../assets/img/buy" alt="" /></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
             </main>
 
         </>
